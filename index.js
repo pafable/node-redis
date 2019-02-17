eb6ae3094099839524f85e5db86d27ea196ec0a6
pafable <39135798+pafable@users.noreply.github.com>
2019-02-17 18:36:02 -0500

    added a line to force crash the app

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const redis = require('redis');
 
+# For debugging unescape, this will crash the app if someone accesses the root route
+const process = require('process');
+
 const app = express();
 const client = redis.createClient({
     host: 'redis-server',
@@ -9,6 +12,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
+    process.exit(0);
     client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
